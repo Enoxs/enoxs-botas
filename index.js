0fc8e113a5f2d4ec255d45f53c7b1aa5b4f953a4
Enoxs <enoxss@gmail.com>
2020-06-17 11:31:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on("ready", async () => {
 
 
 
-
+//////////////////////////////////////////////////////////////////////////////
 client.on("message", async message => {
 
   if (message.author.bot) return;
@@ -52,10 +52,10 @@ client.on("message", async message => {
 
 message.channel.send({embed: {
     color: 114568,
-//    author: {
-//      name: "**__Atnaujinimas__** ",
-//      icon_url: "http://enoxas.lt/discord/naujienos.png"
-//    },
+    author: {
+      name: "**__Atnaujinimas__** ",
+      icon_url: "http://MCSTORY.lt/failai/logo.png"
+    },
     title: antras+" **__Atnaujinimas | Atidarymas__**",
 ///    url: "http://mcstory.lt",
     description: trecias+" Atnaujinimai vyksta.\n"+trecias+" Dar vienas.",
@@ -77,9 +77,6 @@ message.channel.send({embed: {
 	  
   }
 	
-	
-	
-
 	
   if (cmd === `${prefix}taisykles9`){
 
@@ -103,7 +100,32 @@ message.channel.send({embed: {
 });
 	  
   }
-	
+//////////////////////////////////////////////////////////////////////////////
+/////////////  TAISYKLĖS
+//////////////////////////////////////////////////////////////////////////////
+  if (cmd === `${prefix}taisykles9`){
+
+message.channel.send({embed: {
+    color: 114568,
+    title: "<a:laimingas:720259065263030373> **__Perskaitykite ir sutikite su taisyklėmis__**",
+    description: "\nＴＡＩＳＹＫＬＥＳ:\n\n"+
+"<:zole:720289285542248469> 1. Nesikeikti ir neįžeidinėti kitų narių.\n"+
+"<:zole:720289285542248469> 2. Draudžiama apsimesti kitais žmonėmis, jūs esate jūs, o ne kažkas kitas.\n"+
+"<:zole:720289285542248469> 3. Slapyvardžiai, avatarai ir custom statusai turi atitikti etikos taisykles.\n"+
+"<:zole:720289285542248469> 5. Nesireklamuoti ir nereklamuoti įvairaus pobūdžio turinio.\n"+
+"<:zole:720289285542248469> 6. Netrukdyti kitiems. Netaginti žmonių be rimtos priežasties.\n"+
+"<:zole:720289285542248469> 7. Neprašyti rolių, paslaugų ir kitų dalykų kurie jums nepriklauso.\n"+
+"<:zole:720289285542248469> 8. Reakcijų naudojimas nėra skirtas vulgariems žodžiams ar įžeisti žmones.\n"+
+"<:zole:720289285542248469> 9. Nespaminti ir nefloodinti pokalbių, elgtis tvarkingai.\n"+
+"<:zole:720289285542248469> 10. Pokalbiuose neskleisti pašalinių garsų.\n\n"+
+" <:failas:722232975118565387> *Trumpai tariant elkitės tvarkingai ir atsakingai!* <:failas:722232975118565387> \n\n"+
+"\n"+
+"<:ateivis:720293974719332402> **Paspauskite ant <:taip:720291007878660098> emocijos jog sutikti su taisyklėmis!** <:labas:501712009268822016>\n"
+  }
+});
+	  
+  }
+//////////////////////////////////////////////////////////////////////////////	
 	
 	
 	
@@ -127,7 +149,7 @@ message.channel.send({embed: {
 });
 
 
-
+//////////////////////////////////////////////////////////////////////////////
 /*
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
