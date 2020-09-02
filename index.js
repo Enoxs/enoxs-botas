dcb3b0cf10b3cc911543c1d51d8711e9b753f28f
Enoxs <enoxss@gmail.com>
2020-09-02 17:25:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on("ready", async () => {
 class Bot {
     constructor(){
         this.servers = require('./serveriai.json');
-        this.discordClient = Client({sync: true});
+        this.discordClient = new Client({sync: true});
         
         this.discordClient.on("ready", () => {this.initialize();});
         
